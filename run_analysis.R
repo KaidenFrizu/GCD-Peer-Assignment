ff6f50289e357ca2d9efdea7c7183b3f5253d36c
KaidenFrizu <freeezeforward@gmail.com>
2020-06-29 14:22:26 +0800

    Streamlined the extraction of necessary files in dataextract()

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,12 @@
 datacheck <- function(DataFileName = "Data.zip") {
     if(!file.exists("Data.zip")) {
         fileurl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-        download.file(url = fileurl, destfile = DataFileName)
+        message("Downloading file... Please wait")
+        download.file(url = fileurl, destfile = DataFileName, quiet = TRUE)
         downloaddate <<- Sys.time() #Checking download time
         message(paste("Date Downloaded:",downloaddate))
-        Sys.sleep(1)
+        message(paste("Filename:",DataFileName))
+        Sys.sleep(2)
         message("File downloaded successfully")
     }
     else {
@@ -14,17 +16,19 @@ datacheck <- function(DataFileName = "Data.zip") {
 }
 
 dataextract <- function(datazip = "Data.zip") {
-    if(!file.exists("UCI HAR Dataset")) {
-        unzip(datazip)
-        Sys.sleep(1)
-        message("File extracted successfully")
-    }
-    else message(paste(datazip, "is already extracted"))
+    unzip(zipfile =  datazip, files = "UCI HAR Dataset/train/X_train.txt")
+    unzip(zipfile =  datazip, files = "UCI HAR Dataset/train/y_train.txt")
+    unzip(zipfile =  datazip, files = "UCI HAR Dataset/train/subject_train.txt")
+    unzip(zipfile =  datazip, files = "UCI HAR Dataset/test/X_test.txt")
+    unzip(zipfile =  datazip, files = "UCI HAR Dataset/test/y_test.txt")
+    unzip(zipfile =  datazip, files = "UCI HAR Dataset/test/subject_test.txt")
+    Sys.sleep(2)
+    message("Files extracted successfully")
 }
 
 train_tidy <- function() {
     mean_sd_data <- read.table(file = "./UCI HAR Dataset/train/X_train.txt", header = FALSE)[,1:6]
-    activity_train <- read.table(file = "./UCI HAR Dataset/train/Y_train.txt", header = FALSE)
+    activity_train <- read.table(file = "./UCI HAR Dataset/train/y_train.txt", header = FALSE)
     names(activity_train) <- "Activity"
     subject_train <- read.table(file = "./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
     names(subject_train) <- "Subject"
@@ -35,7 +39,7 @@ train_tidy <- function() {
 
 test_tidy <- function() {
     mean_sd_data <- read.table(file = "./UCI HAR Dataset/test/X_test.txt", header = FALSE)[,1:6]
-    activity_test <- read.table(file = "./UCI HAR Dataset/test/Y_test.txt", header = FALSE)
+    activity_test <- read.table(file = "./UCI HAR Dataset/test/y_test.txt", header = FALSE)
     names(activity_test) <- "Activity"
     subject_test <- read.table(file = "./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
     names(subject_test) <- "Subject"
@@ -53,7 +57,8 @@ autobind <- function() {
 }
 
 mean_sd_summary <- function(dataframe) {
-    require(dplyr) #dplyr must be installed to use this function
-    tdata <- dataframe %>% arrange(Subject, Activity)
-    aggregate(. ~ Subject + Activity, data = tdata, FUN = mean)
+    dataframe <- aggregate(. ~ Subject + Activity, data = dataframe, FUN = mean)
+    dataframe$Activity <- as.factor(dataframe$Activity)
+    dataframe$Subject <- as.factor(dataframe$Subject)
+    dataframe
 }
